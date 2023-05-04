19262e94d4f029ffa4a18ed74aa27594745fc507
Andreas Mussgiller <andreas.mussgiller@cern.ch>
2023-05-04 23:36:17 +0200

    Adapted script to new CSV source and format
    - ZIP file with CSV file is downloaded, copied to a temporary file and unzipped to a temporary directory
    - name of CSV data file seems to be regularly changing. The contents of the extracted ZIP file are therefore scanned for the correct filename patter starting with 'API_SP.POP.TOTL_DS2_EN'

--- scripts/process.py
+++ scripts/process.py
@@ -1,30 +1,50 @@
-import urllib
+import urllib.request
 import os
 import csv
 import tempfile
+import zipfile
 
-f = tempfile.NamedTemporaryFile()
-dest = f.name
+tmpfile = tempfile.NamedTemporaryFile(delete=False, suffix='.zip')
+tmpdir = tempfile.TemporaryDirectory()
 
 def download():
-    source = 'http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?format=csv'
-    urllib.urlretrieve(source, dest)
 
+    global filename
+    
+    source = 'http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?downloadformat=csv'
+    
+    with urllib.request.urlopen(source) as response:
+        with open(tmpfile.name, mode="wb") as d:
+            d.write(response.read())
+            tmpfile.close()
+    
+    with zipfile.ZipFile(tmpfile.name, 'r') as zip_ref:
+        zip_ref.extractall(tmpdir.name)
+        tmpfile.close()
+        os.unlink(tmpfile.name)
+    
+    for path in os.scandir(tmpdir.name):
+        if path.is_file():
+            #print(path.name)
+            if path.name.startswith('API_SP.POP.TOTL_DS2_EN'):
+                filename = tmpdir.name + '/' + path.name
+            
 def process():
     # un-pivot the table
-    fo = open(dest)
+    global filename
+    fo = open(filename)
     lines = [ row for row in csv.reader(fo) ]
-    headings = lines[0]
-    lines = lines[1:]
-
+    headings = lines[4]
+    lines = lines[5:]
+    
     outheadings = [ 'Country Name', 'Country Code', 'Year', 'Value' ]
     outlines = []
 
     for row in lines:
-        for idx, year in enumerate(headings[2:]):
-            if row[idx+2]:
+        for idx, year in enumerate(headings[4:]):
+            if row[idx+4]:
                 # do not convert to float as we end up with scientific notation
-                value = row[idx+2]
+                value = row[idx+4]
                 outlines.append(row[:2] + [int(year), value])
 
     writer = csv.writer(open('data/population.csv', 'w'))
